83b3f4097b8c6762b8ff4fefb0fe5c614730b0e2
E132 <107452563+132E@users.noreply.github.com>
2022-07-01 14:39:27 -0500

    make changes to script file

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-const API_KEY = ''
-const DOMAIN = 'https://api.themoviedb.org/3'
-const IMAGE_BASE_PATH = 'https://image.tmdb.org/t/p/original'
+const API_KEY = "19233f7856a875ed08a1ad19d6045200";
+const DOMAIN = "https://api.themoviedb.org/3";
+const IMAGE_BASE_PATH = "https://image.tmdb.org/t/p/original";
+
+const button = document.querySelector("button");
+const searchInput = document.querySelector("#search-input");
+const body = document.querySelector("body");
+
+button.addEventListener("click", async () => {
+  const userInput = searchInput.value;
+  const response = await axios.get(
+    `https://api.themoviedb.org/3/search/movie?query=${searchInput}&api_key=19233f7856a875ed08a1ad19d6045200`
+  );
+  //   console.log(response);
+  const movieList = movie.data.result;
+});
+
+const renderList = (movies) => {
+  movies.forEach((movies) => {
+    const movieID = movies.movieID;
+    console.log(movieID);
+    const title = movies.title;
+    console.log(title);
+  });
+};
